dfd19100bfb22680b7ecb496a639bd8b0d92c21f
bitvcat <shui_mu98@163.com>
2024-02-05 16:25:29 +0800

    improve: 完善readme 和 模板

---
+++ out/client/lua/system.lua
@@ -0,0 +1,28 @@
+
+return {
+  key1 = 1,
+  key2 = 2,
+  key3 = 3,
+  key4 = 4,
+  key5 = 5,
+  array = {
+    [1] = 4,
+    [2] = 5
+  },
+  array1 = {
+    sites = {
+      [1] = {
+        name = "菜鸟教程",
+        url = "www.runoob.com"
+      },
+      [2] = {
+        url = "www.google.com",
+        name = "google"
+      },
+      [3] = {
+        url = "www.weibo.com",
+        name = "微博"
+      }
+    }
+  }
+}
