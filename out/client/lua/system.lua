0779cc22f7b65d11d36b732a63700d6c40c3efec
bitvcat <shui_mu98@163.com>
2024-02-05 17:16:07 +0800

    improve: 纵向表模板修改

--- out/client/lua/system.lua
+++ out/client/lua/system.lua
@@ -1,28 +1,24 @@
 
 return {
   key1 = 1,
-  key2 = 2,
-  key3 = 3,
+  key2 = "fuckyou",
+  key3 = false,
   key4 = 4,
-  key5 = 5,
-  array = {
+  key5 = 3.14,
+  key6 = {
     [1] = 4,
     [2] = 5
   },
-  array1 = {
-    sites = {
-      [1] = {
-        name = "菜鸟教程",
-        url = "www.runoob.com"
-      },
-      [2] = {
-        name = "google",
-        url = "www.google.com"
-      },
-      [3] = {
-        name = "微博",
-        url = "www.weibo.com"
-      }
+  key7 = {
+    [1] = {
+      [1] = 1,
+      [2] = 2,
+      [3] = 3
+    },
+    [2] = {
+      [1] = 4,
+      [2] = 5,
+      [3] = 6
     }
   }
 }
